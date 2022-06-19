13b63bc5665e25b6eee8a6594f80c1aeda2aefbb
Muhammad-me <mmajidhanov@gmail.com>
2022-06-19 12:29:07 +0500

    Mening JSdagi 4-darsim

---
+++ index.js
@@ -0,0 +1,35 @@
+// for(let i =100000000; i >= 0; i--){
+//     console.log(i);
+// }
+
+let students = ["Murodjon", "Mubashsher", "Muhammad", "Azimjon"]
+// const uzunlik = students.length;
+// for(let i = 0; i < uzunlik; i++){
+//     console.log(`Student + ${students[i][0]}`);
+// }
+//! New lesson !//
+//? New lesson ?//
+//* New lesson *//
+
+function chiqar() {
+    let newuser = prompt("Please write your name:");
+    students.push(newuser);
+    console.log(students);
+}
+chiqar()
+
+// function newuser(ism) {
+//     return prompt(`Please write your name`); 
+// }
+// console.log(newuser("Muhammad"));
+
+
+
+
+
+
+
+
+
+
+
